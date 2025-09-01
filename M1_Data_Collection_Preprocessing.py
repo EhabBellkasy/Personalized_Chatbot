f5587ad088b335f617aefdafbc367985ae16774e
EhabBellkasy <38149933+EhabBellkasy@users.noreply.github.com>
2025-09-01 17:31:23 +0300

    {New commit drop here}

--- M1_Data_Collection_Preprocessing.py
+++ M1_Data_Collection_Preprocessing.py
@@ -66,3 +66,62 @@ def f_dataset_List_download(dataSetList = [  'warrenm/movie-dialogues'
 
 # f_dataset_List_download()
 
+
+# dataset = load_dataset("warrenm/movie-dialogues")
+# print(dataset)
+
+
+def f_dataset_download1(dataSetName = "warrenm/movie-dialogues"):
+    try:
+        # cleanedNamed = dataSetName.encode("ascii", "ignore").decode()
+        cleanedNamed = re.sub(r'[^A-Za-z0-9 ]+', '', dataSetName)  # keep only letters, numbers, and spaces
+        excellName = cleanedNamed + ' ' + ".xlsx"
+        jsonName = cleanedNamed + ' ' + ".json"
+        # Load the dataset (example: Cornell Movie Dialogues)
+        dataset = load_dataset(dataSetName)
+
+        # Save each split (train, test, validation) into its own JSON file
+        for split in dataset.keys():
+            dataset[split].to_json(f"{cleanedNamed}_{split}.json")
+
+        print("✅ All dataset splits saved as JSON files!")
+    except:
+        print("something wrong with "+ dataSetName)
+
+
+
+f_dataset_download1(dataSetName = 'fka/awesome-chatgpt-prompts')
+
+
+
+dataSetList = [  'warrenm/movie-dialogues' 
+                                            ,'fka/awesome-chatgpt-prompts'
+                                            ,'mylesmharrison/cornell-movie-dialog'
+                                            ,'cornell-movie-dialog/cornell_movie_dialog'
+                                            ,'spawn99/CornellMovieDialogCorpus'
+                                            ,'google/Synthetic-Persona-Chat'
+                                            ,'bavard/personachat_truecased'
+                                            ,'AlekseyKorshuk/persona-chat'
+                                            ,'racineai/OGC_Energy_Arabic'
+                                            ,'silma-ai/arabic-broad-benchmark'
+                                            ,'bitext/Bitext-customer-support-llm-chatbot-training-dataset'
+                                            ,'Victorano/Bitext-customer-support-llm-chatbot-testing-dataset-seed42-4k-4.5k'
+                                            ,'Tobi-Bueck/customer-support-tickets'
+                                            ,'Kaludi/Customer-Support-Responses'
+                                            ,'warabimoti/pop_entertainment'
+                                            ,'JYouthCultureQA/POP_entertainment'
+                                            ,'DeepNLP/ai-agent-entertainment'
+                                            ,'DataDump1/entertainment_reddit'   
+                                            ]
+
+
+
+
+
+for dataSetIter in dataSetList :
+            print('*'*100)
+            print('*'*100)
+            print(dataSetIter)
+            print('*'*100)
+            print('*'*100)
+            f_dataset_download1(dataSetName = dataSetIter)
